43772bf0583040fca6399cffdd611c4087dfa64f
djso89 <danielso89@gmail.com>
2020-09-14 23:38:45 -0700

    task2

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""  script to export data in the CSV format."""
+
+import csv
+import requests
+from sys import argv
+
+if __name__ == '__main__':
+    user_json = requests.get(
+        "https://jsonplaceholder.typicode.com/users/{}/".format(argv[1]))
+
+    tasks_json = requests.get(
+        "https://jsonplaceholder.typicode.com/users/{}/todos"
+        .format(argv[1])).json()
+
+    usr_info_dict = {}
+    usr_info_dict = ["{}".format(argv[1])] = []
+    for x in tasks_json:
+        usr_info_dict["{}".format(argv[1])].append({
+            'task': x.get("title"),
+            'completed': x.get('completed'),
+            'username': user_json.json().get("username")
+        })
+
+    with open("{}.csv".format(argv[1]), 'w') as file:
+        json.dump(usr_info_dict, file)
