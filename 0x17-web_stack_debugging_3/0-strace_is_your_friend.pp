b815f357ecbba4d389d2116b63ebe4c274dd54a1
djso89 <danielso89@gmail.com>
2020-09-30 23:09:06 -0700

    task 0

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,2 @@
 # fix the typo in the config file
-exec { 'replace':
-command	=> "sed -i 's/phpp/php/g' /var/www/html/wp-settings.php",
-path	=> '/bin',
-}
+exec { '/usr/bin/env sudo sed -i "s/phpp/php/g" /var/www/html/wp-settings.php': }
