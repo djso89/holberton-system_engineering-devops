1c31c4d5c6dec9b5f8b0adecbac3cc29127a4427
djso89 <danielso89@gmail.com>
2020-10-08 20:43:22 -0700

    forgot commas

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,10 +1,10 @@
 #change the Open file limit for holberton
 exec { 'set soft limit to 2000':
-  path    => '/bin'
+  path    => '/bin',
   command => "sed -i 's/4/2000' /etc/security/limits.conf"
 }
 
 exec { 'set hard limit to 4000':
-  path    => '/bin'
+  path    => '/bin',
   command => "sed -i 's/5/4000/' /etc/security/limits.conf"
 }
