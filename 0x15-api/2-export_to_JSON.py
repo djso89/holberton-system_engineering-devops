442bb6492833dd971353e4fe200961e0dbaeff3b
djso89 <danielso89@gmail.com>
2021-05-03 16:16:03 -0700

    fixed doc format

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """ script to export data in the json format."""
-
 import json
 import csv
 import requests
 from sys import argv
+
 
 if __name__ == '__main__':
     user_json = requests.get(
